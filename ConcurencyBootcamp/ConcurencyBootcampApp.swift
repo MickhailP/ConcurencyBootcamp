6e2b39c366f260b2bc7c1337542c5325d114445c
Mikhail <mich_p@inbox.ru>
2022-10-11 19:46:20 +0300

    Initial Commit

---
+++ ConcurencyBootcamp/ConcurencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurencyBootcampApp.swift
+//  ConcurencyBootcamp
+//
+//  Created by Миша Перевозчиков on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConcurencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurencyBootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConcurencyBootcamp
+//
+//  Created by Миша Перевозчиков on 11.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
